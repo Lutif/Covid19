15d3c44bf63e2530c9a71548a290306aaee54bb4
lutif <lmandhro@gmail.com>
2020-03-27 21:04:35 +0500

    Updates

---
+++ precache-manifest.65c5240c589d6beb87f060988ad7574e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8e62b2279927b5a05759ef3db45958",
+    "url": "/Covid19/index.html"
+  },
+  {
+    "revision": "9a805a43e4b28f580628",
+    "url": "/Covid19/static/css/main.5ce39254.chunk.css"
+  },
+  {
+    "revision": "cf031f7780156c3d8147",
+    "url": "/Covid19/static/js/2.d78a1deb.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/Covid19/static/js/2.d78a1deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a805a43e4b28f580628",
+    "url": "/Covid19/static/js/main.f6e59779.chunk.js"
+  },
+  {
+    "revision": "d597ef8edb7a0b2a0114",
+    "url": "/Covid19/static/js/runtime-main.fb07a569.js"
+  }
+]);
